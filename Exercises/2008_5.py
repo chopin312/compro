7ec2f8b9136bdebad9bb14bfeef88023bac6047c
chopin312 <chopin3122544@gmail.com>
2021-08-20 19:38:50 +0700

    Update Exercise 1.5

--- Exercises/2008_5.py
+++ Exercises/2008_5.py
@@ -1,4 +1,4 @@
 #5. Input the user's name and age and outputs a sentence containing them.
-
+Name =  input("Input your name : ")
 Age = input("Input your age : ")
-print("So, your age is", Age,"so old.")
+print("So, your name is", Name, "and your age is", Age,"years old.")
