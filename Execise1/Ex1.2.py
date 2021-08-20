14ab6a31be996718d5d25e45ed0966f547e44518
chopin312 <chopin3122544@gmail.com>
2021-08-20 14:22:49 +0700

    Add folder Exercise + Ex1.2

---
+++ Execise1/Ex1.2.py
@@ -0,0 +1,6 @@
+#2. Computes and prints the area of a rectangle, given an input width and height
+
+W = float(input("input width : "))
+H = float(input("input height : "))
+A = W*H
+print("rectangle area : "+ str(A))
